e87fc2d9dc1f768e1f1eac20a4ba1222cebb50b2
Christopher <chrisrw300@gmail.com>
2020-08-30 12:11:40 -0700

    added time tracker for different classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,34 @@ var currentDay = moment().format("ddd, MMMM Do YYYY");
 var currentTime = moment().format("h:mm:ss a");
 $("#currentDay").append(currentDay);
 $("#currentTime").append(currentTime);
+
+
+function timeTracker() {
+    //get current number of hours.
+    var realTime = moment().hour();
+
+    //splits time blocks to 
+    $(".time-block").each(function () {
+        var scheduleHour = parseInt($(this).attr("id").split("hour-")[1]);
+
+        //if block of time is less than the current hour, change class to past
+        if (scheduleHour < realTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+        //if block of time equals the current hour, change class to present
+        else if (scheduleHour === realTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        //if block of time more than the current hour, change class to past
+        else if (scheduleHour > realTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+}
+timeTracker();
